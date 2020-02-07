dedaa8beb0112c0b02e2135f781eb9c5bd54921a
Cuvelier Mathias <m.cuvelier@rt-iut.re>
2020-02-07 13:55:28 +0400

    Exercice 1 1 à 1 3

--- src/tp1/Client.java
+++ src/tp1/Client.java
@@ -1,6 +1,20 @@
 package tp1;
 
 public class Client {
-	static String nom, prenom;
-
+	//Attributs
+	public String nom, prenom ; 
+	
+	//Constructeurs
+	public Client(String n, String p){
+		nom = n; 
+		prenom = p;
+	}
+	//Accesseurs
+	public String getNom() {
+		return nom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
 }
