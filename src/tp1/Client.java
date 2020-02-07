e244167a9bfe0c03d1aced21bfe736785325e84c
Cuvelier Mathias <m.cuvelier@rt-iut.re>
2020-02-07 15:43:02 +0400

    exo 3 3 fini

--- src/tp1/Client.java
+++ src/tp1/Client.java
@@ -2,12 +2,14 @@ package tp1;
 
 public class Client {
 	//Attributs
-	public String nom, prenom ; 
+	public String nom, prenom ;
+	private Compte compteCourant;
 	
 	//Constructeurs
-	public Client(String n, String p){
+	public Client(String n, String p, Compte compte){
 		nom = n; 
 		prenom = p;
+		this.compteCourant = compte;
 	}
 	//Accesseurs
 	public String getNom() {
--- src/tp1/TestClient.java
+++ src/tp1/TestClient.java
@@ -4,10 +4,17 @@ public class TestClient {
 	public static void main(String[] args) {
 		
 		//Declaration
-		Client client = new Client("Cuvelier","Mathias");
+		Client client ;
+		Compte compte1 ;
+		
+		//Exercice3
+		compte1 = new Compte(1);
+		client = new Client("Cuvelier","Mathias", compte1);
 		
 		//Exercice 1.4
 		System.out.println("Le nom est " + client.getNom());
 		System.out.println("Le prenom est " + client.getPrenom());
+		//Exercice 3.3
+		System.out.println("Compte numero " + compte1.getNumero());
 	}
 }
