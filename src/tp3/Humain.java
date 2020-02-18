aa12937a28e6002a4b9bee06983ec4a3a92b098a
Cuvelier Mathias <m.cuvelier@rt-iut.re>
2020-02-18 23:22:40 +0400

    début du tp3 et exo1 fini

---
+++ src/tp3/Humain.java
@@ -0,0 +1,42 @@
+package tp3;
+
+public class Humain {
+
+	//Attribruts
+	//Exercice 1.1
+	protected String nom, boissonpref;
+
+	//Constructeurs
+
+	//Exercice1.2
+	public 	Humain(String nom) {
+		this.nom=nom;
+		boissonpref = "lait";
+	}
+
+	//Methodes
+
+		//Exercice1.3
+	public String quelEstTonNom() {
+		return nom;
+	}
+
+	public String  quelleEstTaBoisson() {
+		return boissonpref;
+	}
+
+		//exercice 1.4
+	public void parler(String texte) {
+		System.out.println("( " + nom + " ) " + " - " + texte);
+	}
+
+	//Exercice 1.5
+	public void sePresenter() {
+		this.parler("Bonjour, je suis " + this.quelEstTonNom() + " et ma boisson preferee est le " + this.quelleEstTaBoisson());
+	}
+
+	//Exercice 1.6
+	public void boire() {
+		this.parler("Ah ! un bon verre de " + this.quelleEstTaBoisson() + " ! GLOUPS ! ");
+	}
+}
